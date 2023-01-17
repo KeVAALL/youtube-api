b45d332355760b68de99bb3c69fca597275077e9
Keval Khatri <kevalkhatri32@gmail.com>
2023-01-17 14:53:19 +0530

    Resolved - Cannot access API Key from .env

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import "./App.css";
 const YOUTUBE_PLAYLIST_API =
   "https://www.googleapis.com/youtube/v3/playlistItems";
 
+// const YOUTUBE_API_KEY = "AIzaSyAIQSW3lDhMrNJX9ijFaAmF2MJSHTqgS-0";
+
 export async function getServerData() {
-  const response = await fetch(YOUTUBE_PLAYLIST_API);
+  const response = await fetch(
+    `${YOUTUBE_PLAYLIST_API}?key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+  );
 
   const data = await response.json();
 
