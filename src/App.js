3a466f008ad2537d27f22f4a10e6264042eebcd5
Keval Khatri <kevalkhatri32@gmail.com>
2023-01-17 15:48:07 +0530

    Added frontend part to show the data fetched

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-const YOUTUBE_PLAYLIST_API =
-  "https://www.googleapis.com/youtube/v3/playlistItems";
-
-// const YOUTUBE_API_KEY = "AIzaSyAIQSW3lDhMrNJX9ijFaAmF2MJSHTqgS-0";
-
-export async function getServerData() {
-  const response = await fetch(
-    `${YOUTUBE_PLAYLIST_API}?key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
-  );
-
-  const data = await response.json();
-
-  console.log(data);
-}
-
 function App() {
+  const [youtubeData, setYoutubeData] = useState("");
+
+  const YOUTUBE_PLAYLIST_API =
+    "https://www.googleapis.com/youtube/v3/playlistItems";
+
+  async function getServerData() {
+    try {
+      const response = await fetch(
+        `${YOUTUBE_PLAYLIST_API}?part=snippet&maxResults=50&playlistId=PLC3y8-rFHvwiaOAuTtVXittwybYIorRB3&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+      );
+
+      const data = await response.json();
+
+      console.log(data);
+      setYoutubeData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     getServerData();
   }, []);
 
   return (
     <div className="App">
-      <h1>Youtube API</h1>
+      <h1>My Playlists</h1>
+
+      <ul>
+        {youtubeData.items.map((item) => {
+          const { id, snippet = {} } = item;
+          const { title, thumbnails = {} } = snippet;
+          const { medium = {} } = thumbnails;
+
+          return (
+            <li key={id}>
+              <a>
+                <p>
+                  <img
+                    width={medium.width}
+                    height={medium.height}
+                    src={medium.url}
+                    alt=""
+                  />
+                </p>
+                <h3>{title}</h3>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
